69ae5f0bb4ce15580a48938916407abf7ec3aaf6
rahulverman121 <98263712+rahulverman121@users.noreply.github.com>
2022-03-17 01:35:06 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import Header from './Header';
+import Header from './header';
+import Getstarted from './Getstarted';
 import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <Header />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Howitwork from './Howitwork';
+import Menu from './Menu';
+import Questions from './Questions';
+import Footer from './Footer';
+<div>
+<div ClassName="title">
+      <p>Header.....</p>
+     </div>
+     </div>
+ReactDOM.render(<><Header /><br></br><Getstarted/><Howitwork /><Menu /><Questions/><Footer /></> , document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
