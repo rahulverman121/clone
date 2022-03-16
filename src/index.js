271806ed1976217849a54831eade113ab9f444d0
rahulverman121 <98263712+rahulverman121@users.noreply.github.com>
2022-03-17 01:41:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Header from './header';
+import App from './App';
+import Header from './Header';
 import Getstarted from './Getstarted';
 import reportWebVitals from './reportWebVitals';
 import Howitwork from './Howitwork';
 import Menu from './Menu';
 import Questions from './Questions';
 import Footer from './Footer';
-<div>
-<div ClassName="title">
-      <p>Header.....</p>
-     </div>
-     </div>
-ReactDOM.render(<><Header /><br></br><Getstarted/><Howitwork /><Menu /><Questions/><Footer /></> , document.getElementById("root"));
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Header />
+    <Getstarted />
+    <Howitwork />
+    <Menu />
+    <Questions />
+    <Footer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
