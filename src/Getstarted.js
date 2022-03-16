5e9b807922448e773322d046188fb8c289646da7
rahulverman121 <98263712+rahulverman121@users.noreply.github.com>
2022-03-17 01:53:15 +0530

    Update Getstarted.js

--- src/Getstarted.js
+++ src/Getstarted.js
@@ -7,7 +7,7 @@ function Getstarted()  {
             <div className="blog">
                 <h1 className="starttitle">Hot, freshly-cooked meals delivered to your door at just $7.59/meal!</h1>
                 Get meals from Halifax's top restaurants and chefs delivered twice daily.<br/><br/><br/>
-                <a href="#" className="button">Get started now</a>
+                <a href="www.getstarted.com" className="button">Get started now</a>
             </div>
             <div className="image">
                 <img src={getstarted} alt="Get Started Meal"></img>
