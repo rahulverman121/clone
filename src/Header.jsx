e4d740b781b3390047e3dd7a1ce5b9c75d0fc339
rahulverman121 <98263712+rahulverman121@users.noreply.github.com>
2022-03-18 01:09:25 +0530

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -13,6 +13,7 @@ function Header() {
     <ul className="right">
     <li>Log in </li>
     <li><button>Sign up</button></li>
+        <li class="navbtn">_ <br/>_<br/> _</li>
      </ul>
      </div>
     );
